dd860d0b96aa46faded555622a33120fa34c4eb2
Fede77git <fedeecarp11@gmail.com>
2026-02-22 18:39:51 -0300

    interruptor puerta

---
+++ WiredUp_Game/Assets/Scripts/InterruptorPresion.cs
@@ -0,0 +1,52 @@
+using UnityEngine.Events;
+using UnityEngine;
+
+public class InterruptorPresion : MonoBehaviour
+{
+
+    //visual
+    public Renderer meshBoton;
+    public Color colorApagado = Color.red;
+    public Color colorEncendido = Color.green;
+
+    //eventos
+    public UnityEvent alPresionar;
+    public UnityEvent alSoltar;
+
+    private int objetosEncima = 0;
+
+    void Start()
+    {
+        if (meshBoton != null) meshBoton.material.color = colorApagado;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        
+        if (other.CompareTag("Caja") || other.CompareTag("Player"))
+        {
+            objetosEncima++;
+            if (objetosEncima == 1) 
+            {
+                if (meshBoton != null) meshBoton.material.color = colorEncendido;
+                alPresionar.Invoke();
+                Debug.Log("¡Interruptor ACTIVADO!");
+            }
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Caja") || other.CompareTag("Player"))
+        {
+            objetosEncima--;
+            if (objetosEncima <= 0)
+            {
+                objetosEncima = 0;
+                if (meshBoton != null) meshBoton.material.color = colorApagado;
+                alSoltar.Invoke();
+                Debug.Log("Interruptor APAGADO.");
+            }
+        }
+    }
+}
