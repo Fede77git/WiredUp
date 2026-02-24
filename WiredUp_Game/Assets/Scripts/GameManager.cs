6a1c8c7da43335eab191ef810e06f534848b3dd8
Fede77git <fedeecarp11@gmail.com>
2026-02-23 21:45:30 -0300

    game manager, build 0,.2

---
+++ WiredUp_Game/Assets/Scripts/GameManager.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class GameManager : MonoBehaviour
+{
+    
+
+   
+    void Update()
+    {
+        
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+    }
+}
